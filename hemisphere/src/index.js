64694c42a1c4072260752df2f4f5af8d35ea0a3e
D-YF <daiyf1028@gmail.com>
2023-08-27 16:06:38 -0500

    second way to initialize state

--- hemisphere/src/index.js
+++ hemisphere/src/index.js
@@ -3,12 +3,20 @@ import {createRoot} from 'react-dom/client';
 import HemisphereDisplay from './HemisphereDisplay';
 
 class App extends React.Component {
+
+    // Second way to initialize state
+    state = {latitude: null, longitude: null, errorMessage: ''}
+
+    /*
     constructor(props) {
         super(props) // You can't use "this.sth" until you call parent's constructor by calling super(props)
 
-        // One of the ways to initialize
-        this.state = { latitude: null, longitude: null, errorMessage: '' }
-
+        // One of the ways to initialize state
+        // this.state = { latitude: null, longitude: null, errorMessage: '' }
+    }
+    */
+   
+    componentDidMount() {
         window.navigator.geolocation.getCurrentPosition(
             (position) => {
                 this.setState({ latitude: position.coords.latitude,
@@ -18,7 +26,6 @@ class App extends React.Component {
                 this.setState({ errorMessage: error.message })
             }
         )
-
     }
 
     render() {
@@ -26,7 +33,7 @@ class App extends React.Component {
             return (<div> {this.state.errorMessage} </div>)
         }
         else if(!this.state.errorMessage && this.state.latitude){
-            return (<div> {this.state.latitude} </div>)
+            return (<div> <HemisphereDisplay latitude={this.state.latitude}/> </div>)
         }
         else
             return ( <div> Loading... </div>)
