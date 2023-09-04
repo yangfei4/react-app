80215235b1add6e3a5a9771f83fa4df825bd34ad
D-YF <daiyf1028@gmail.com>
2023-09-04 11:55:07 -0500

    useContext hook in functional components

--- contextapi/src/component/TodoList.js
+++ contextapi/src/component/TodoList.js
@@ -1,28 +1,42 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import { ThemeContext } from '../contexts/ThemeContext';
 
-class TodoList extends React.Component{
-    // static contextType = ThemeContext;
+const TodoList = () => {
+    const {isDarkTheme, lightTheme, darkTheme, changeTheme} = useContext(ThemeContext);
+    const theme = (isDarkTheme) ? darkTheme : lightTheme;
 
-    render() {
-        return (
-            <ThemeContext.Consumer>{(context) => {
-                const {isDarkTheme, lightTheme, darkTheme, changeTheme} = context;
-                const theme = (isDarkTheme) ? darkTheme : lightTheme;
-                console.log(theme);
-        
-                return(
-                    <div style={{background: theme.background, color: theme.text, height: '140px', textAlign: 'center'}} className='ui list'>
-                        <p className='item'>Plan a family trip</p>
-                        <p className='item'>Date with a girl</p>
-                        <p className='item'>Go for a walk</p>
-                        <button className='ui button primary' onClick={changeTheme}>Change the theme</button>
-                    </div>
-                )
-            }}
-            </ThemeContext.Consumer>
-        )
-    }
+    return(
+        <div style={{background: theme.background, color: theme.text, height: '140px', textAlign: 'center'}} className='ui list'>
+            <p className='item'>Plan a family trip</p>
+            <p className='item'>Date with a girl</p>
+            <p className='item'>Go for a walk</p>
+            <button className='ui button primary' onClick={changeTheme}>Change the theme</button>
+        </div>
+    )
 }
+
+// class TodoList extends React.Component{
+//     // static contextType = ThemeContext;
+
+//     render() {
+//         return (
+//             <ThemeContext.Consumer>{(context) => {
+//                 const {isDarkTheme, lightTheme, darkTheme, changeTheme} = context;
+//                 const theme = (isDarkTheme) ? darkTheme : lightTheme;
+//                 console.log(theme);
+        
+//                 return(
+//                     <div style={{background: theme.background, color: theme.text, height: '140px', textAlign: 'center'}} className='ui list'>
+//                         <p className='item'>Plan a family trip</p>
+//                         <p className='item'>Date with a girl</p>
+//                         <p className='item'>Go for a walk</p>
+//                         <button className='ui button primary' onClick={changeTheme}>Change the theme</button>
+//                     </div>
+//                 )
+//             }}
+//             </ThemeContext.Consumer>
+//         )
+//     }
+// }
 
 export default TodoList;
