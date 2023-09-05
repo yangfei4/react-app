54a2c06448cd2feca2b0c7b879c5265ed8dca797
D-YF <daiyf1028@gmail.com>
2023-09-05 17:09:29 -0500

    basics of nodejs including functinos, eventEmitter, util inherit

--- node-js/app.js
+++ node-js/app.js
@@ -1,1 +1,77 @@
-console.log("hello world");
+const events = require('events');
+const util = require("util");
+
+const teams = function(name){
+    this.name = name;
+}
+
+util.inherits(teams, events.EventEmitter); // inherit from superConstructor(2-nd arg)
+
+const Arenal = new teams("Arenal");
+const Juventus = new teams("Juventus");
+const Galatasaray = new teams("Galatasaray");
+
+const teamArray = [ Arenal, Juventus, Galatasaray ];
+
+teamArray.forEach((team) => {
+    team.on('nation', (nation) => { // event name; event listener
+        console.log(team.name + ' is ' + nation + ' football team!');
+    })
+})
+
+Arenal.emit('nation', 'English');
+Juventus.emit('nation', 'Italian');
+Galatasaray.emit('nation', 'Turkish');
+
+/* ******************** 
+
+const events = require('events');
+const myEmitter = new events.EventEmitter;
+
+myEmitter.on('test', function(arg){
+    console.log(arg);
+});
+
+myEmitter.emit('test', 'hello world');
+
+ ******************** */
+
+// const helper = require('./helper');
+
+// console.log(helper);
+// console.log(helper.user("Alex"));
+// console.log(helper.id("my id"));
+
+// console.log("hello world");
+
+// let count = 0;
+
+// const timer = setInterval(() => {
+//     count = count + 2;
+//     console.log(`${count}`);
+//     if(count > 10){
+//         clearInterval(timer);
+//     }
+// }, 2000);
+
+// console.log(__dirname);
+// console.log(__filename);
+
+// function orange() {
+//     console.log("hello");
+// }
+
+// orange();
+
+// let red = function() {
+//     console.log("hi there");
+// }
+
+// red();
+
+// function callbackFunc(func){
+//     console.log(func);
+//     func();
+// }
+
+// callbackFunc(red);
---
+++ node-js/helper.js
@@ -0,0 +1,17 @@
+const user = function(date){
+    return `${date} is logged in.`
+}
+
+const id = function(id){
+    return `${id}`
+}
+
+const email = function(email){
+    return `${email}`
+}
+
+module.exports = {
+    user, 
+    id, 
+    email
+};
