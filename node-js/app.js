fe059d8f07ebdc8cdcaba9bed53e46bda13c8d64
D-YF <daiyf1028@gmail.com>
2023-09-06 21:09:11 -0500

    express with ejs: middleware, query string and post request

--- node-js/app.js
+++ node-js/app.js
@@ -1,103 +1,32 @@
-const http = require("http");
-const fs = require("fs");
+const express = require("express");
+const { nextTick } = require("process");
+const bodyParser = require("body-parser");
+const urlencodedParser = bodyParser.urlencoded( { extended: false });
 
-const server = http.createServer((req, res) => {
-    if(req.url === '/home' || req.url === '/'){
-        res.writeHead(200, { 'Content-type': 'text/html' });
-        fs.createReadStream(__dirname+'/index.html').pipe(res);
-    }
-    else if(req.url === '/about'){
-        res.writeHead(200, { 'Content-type': 'text/html'});
-        fs.createReadStream(__dirname+'/about.html').pipe(res);
-    }
-    else{
-        res.writeHead(200, { 'Content-type': 'text/html'});
-        fs.createReadStream(__dirname+'/404.html').pipe(res);
-    }
- 
-})
+const app = express();
 
-server.listen(3000, '127.0.0.1');
+app.set("view engine", "ejs");
 
-/* ******************** 
+app.use("/assets", express.static('assets'));
 
-const events = require('events');
-const util = require("util");
-
-const teams = function(name){
-    this.name = name;
-}
-
-util.inherits(teams, events.EventEmitter); // inherit from superConstructor(2-nd arg)
-
-const Arenal = new teams("Arenal");
-const Juventus = new teams("Juventus");
-const Galatasaray = new teams("Galatasaray");
-
-const teamArray = [ Arenal, Juventus, Galatasaray ];
-
-teamArray.forEach((team) => {
-    team.on('nation', (nation) => { // event name; event listener
-        console.log(team.name + ' is ' + nation + ' football team!');
-    })
-})
-
-Arenal.emit('nation', 'English');
-Juventus.emit('nation', 'Italian');
-Galatasaray.emit('nation', 'Turkish');
-
- ******************** */
-
-
-/* ******************** 
-
-const events = require('events');
-const myEmitter = new events.EventEmitter;
-
-myEmitter.on('test', function(arg){
-    console.log(arg);
+app.get('/', (req, res) => {
+    res.render("index");
 });
 
-myEmitter.emit('test', 'hello world');
+app.get('/about', (req, res) => {
+    res.render("about", {qs: req.query});
+});
 
- ******************** */
+app.post('/about', urlencodedParser, (req, res) => {
+    console.log(req.body);
+    res.render("about-success", { data: req.body});
+})
 
-// const helper = require('./helper');
+app.get('/profile/:name', (req, res) => {
+    const data = { age: 22, job: "developer" , friends: ["Ian", "Alex", "Ruining"]};
+    res.render("profile", { person: req.params.name, data });å
+})
 
-// console.log(helper);
-// console.log(helper.user("Alex"));
-// console.log(helper.id("my id"));
-
-// console.log("hello world");
-
-// let count = 0;
-
-// const timer = setInterval(() => {
-//     count = count + 2;
-//     console.log(`${count}`);
-//     if(count > 10){
-//         clearInterval(timer);
-//     }
-// }, 2000);
-
-// console.log(__dirname);
-// console.log(__filename);
-
-// function orange() {
-//     console.log("hello");
-// }
-
-// orange();
-
-// let red = function() {
-//     console.log("hi there");
-// }
-
-// red();
-
-// function callbackFunc(func){
-//     console.log(func);
-//     func();
-// }
-
-// callbackFunc(red);
+app.listen(3000, () => {
+    console.log("listen to port 3000");
+});
