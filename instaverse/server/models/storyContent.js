43d8b3dfabcb3fb89812ce450a61971b8118140f
D-YF <daiyf1028@gmail.com>
2023-09-16 20:53:59 -0500

    update story schema, userId and likes array

--- instaverse/server/models/storyContent.js
+++ instaverse/server/models/storyContent.js
@@ -3,10 +3,10 @@ import mongoose from "mongoose";
 const storySchema = mongoose.Schema({
     caption: { type: String, required: true },
     username: { type: String, required: true },
-    // userId: { type: String, required: true },
+    userId: { type: String, required: true }, // to be updated to required
     image: { type: String, required: true },
     tags: String,
-    likes: { type: Number, default: 0 },
+    likes: { type: [String], default: [] },
     postDate: { type: Date, default: new Date() }
 });
 
