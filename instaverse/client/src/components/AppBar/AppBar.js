c683479f0a51cb00813ef8477fb022885d48a55e
D-YF <daiyf1028@gmail.com>
2023-09-16 20:47:33 -0500

    frontend for dynamic appBar

--- instaverse/client/src/components/AppBar/AppBar.js
+++ instaverse/client/src/components/AppBar/AppBar.js
@@ -1,16 +1,40 @@
-import React from 'react';
-import { Link } from "react-router-dom";
+import React, { useState, useEffect } from 'react';
+import { Link, useNavigate, useLocation } from "react-router-dom";
 import { Layout, Image, Typography, Avatar, Button } from "antd";
+import { useDispatch } from "react-redux";
+
 import Logo from "../../images/Instaverse.png";
 import style from "./style";
+import { LOGOUT } from '../../constants/actionTypes';
+import decode from "jwt-decode";
 
 const { Title } = Typography;
 const { Header } = Layout;
 
 export default function AppBar() {
-    const user = null;
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+    const location = useLocation();
+    const [user, setUser] = useState(JSON.parse(localStorage.getItem("profile")));
 
-  return (
+    useEffect(()=>{
+        const token = user?.token;
+
+        if (token){
+            const decodedToken = decode(token);
+            if (decodedToken.exp * 1000 < new Date().getTime()) logout();
+        };
+
+        setUser(JSON.parse(localStorage.getItem("profile")));
+    }, [location]); // when url changes, set the user
+
+    const logout = () => {
+        dispatch({ type: LOGOUT });
+        navigate("/authform");
+        setUser(null);
+    }
+
+    return (
     <Header style={style.header}>
         <Link to="/" >
             <div style={style.homeLink}>
@@ -28,12 +52,12 @@ export default function AppBar() {
         ) : (
             <div style={style.userInfo}>
                 <Avatar style={style.avatar} alt="username" size="large">
-                    Y
+                    {user?.result?.username?.charAt(0)?.toUpperCase()}
                 </Avatar>
                 <Title style={style.title} level={4}>
-                    Yangfei4
+                    {user?.result?.username}
                 </Title>
-                <Button htmlType='button'>
+                <Button onClick={logout} htmlType='button'>
                     Log Out
                 </Button>
             </div>
