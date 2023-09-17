7ac1a923d5f08210acb91c694e31e4c23d61045e
D-YF <daiyf1028@gmail.com>
2023-09-16 20:50:58 -0500

    changed to constant variables

--- instaverse/client/src/reducers/stories.js
+++ instaverse/client/src/reducers/stories.js
@@ -1,14 +1,19 @@
+import {
+    FETCH_ALL_STORIES,
+    CREATE_STORY,
+    UPDATE_STORY,
+    DELETE_STORY
+} from "../constants/actionTypes";
+
 const storyReducer = (state = [], action) => {
     switch(action.type){
-        case "FETCH_ALL_STORIES":
+        case FETCH_ALL_STORIES:
             return action.payload;
-        case "CREATE_STORY":
+        case CREATE_STORY:
             return [...state, action.payload];
-        case "UPDATE_STORY":
+        case UPDATE_STORY:
             return state.map((story) => story._id === action.payload._id ? action.payload : story );
-        case "LIKE_STORY":
-            return state.map((story) => story._id === action.payload._id ? action.payload : story );
-        case "DELETE_STORY":
+        case DELETE_STORY:
             return state.filter((story) => story._id !== action.payload);
         default:
             return state;
