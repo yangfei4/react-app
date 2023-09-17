6aa11a9faeab1f8c93290a5d132dc17533a49644
D-YF <daiyf1028@gmail.com>
2023-09-16 20:53:10 -0500

    fixed request by adding async keywords

--- instaverse/server/controllers/users.js
+++ instaverse/server/controllers/users.js
@@ -5,7 +5,7 @@ import User from "../models/user.js";
 const login = async (req, res) => {
     const { email, password } = req.body;
     try {
-        const oldUser = User.findOne({ email });
+        const oldUser = await User.findOne({ email });
 
         if(!oldUser){
             return res.status(404).json({ msg: "User does not exist" });
@@ -28,11 +28,11 @@ const signup = async (req, res) => {
     const { username, email, password, confirmPassword } = req.body;
 
     try {
-        const oldUser = User.findOne({email});
+        const oldUser = await User.findOne({email}); //must have await
+
         if (oldUser){
             return res.status(400).json({ msg: "Email already exists" });
         }
-
         if (password !== confirmPassword){
             return res.status(400).json({ msg: "Passwords do not match" });
         }
@@ -40,7 +40,6 @@ const signup = async (req, res) => {
         const encryptedPassword = await bcrypt.hash(password, 12);
         const result = await User.create({username, email, password: encryptedPassword});
         const token = jwt.sign({ email: result.email, id: result._id }, "1234", { expiresIn: "1h" });
-
         res.status(201).json({ result, token });
 
     } catch (error) {
